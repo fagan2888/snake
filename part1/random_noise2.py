33e42565b9997ec7728bf339597a0e5ead8c5029
mdnu <mark@sdentertainment.ca>
2016-10-01 21:20:03 -0400

    begun introduction, part 3 of 13.

---
+++ part1/random_noise2.py
@@ -0,0 +1,22 @@
+from random import normalvariate, uniform
+import matplotlib.pyplot as plt
+
+def generate_data(n, generator_type):
+    ''' (number, random) -> array
+    
+    Returns an array of n-many epsilon values taken from
+    the distribution specified by generator type.
+    '''
+    
+    epsilon_values = []
+    for i in range(n):
+        if generator_type == 'U':
+            e = uniform(0, 1)
+        else:
+            e = normalvariate(0, 1)
+        epsilon_values.append(e)
+    return epsilon_values
+
+data = generate_data(100, 'U')
+plt.plot(data, 'b-')
+plt.show()
